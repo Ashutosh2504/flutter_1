fa10c32804ac20d1266e823fc6df37ac817c2fb1
Ashutosh2504 <ashup953@gmail.com>
2023-07-06 21:45:52 +0530

    day2 learned scaffold and its appBar, body, footer

---
+++ lib/home_page.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  final int days = 30;
+  final String name = "Ashutosh's";
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Health App'),
+      ),
+      body: Center(
+        child: Container(
+          child: Text('Welcome to $days days of $name flutter'),
+        ),
+      ),
+      drawer: Drawer(),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_1/home_page.dart';
 
 void main() {
   runApp(MyApp());
@@ -9,14 +10,14 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    int days = 30;
+    String name = "Ashutosh's";
+    num temp = 32.5;
+    var day = "Thursday";
+    const pie = 3.14;
+
     return MaterialApp(
-      home: Material(
-        child: Center(
-          child: Container(
-            child: Text('Ashutosh world for flutter'),
-          ),
-        ),
-      ),
+      home: HomePage(),
     );
   }
 }
